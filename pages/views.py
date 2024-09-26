e470dd45d84bdabbe2f0d9025585bd67d0b6abde
Jules Stier <juliostier1128@gmail.com>
2024-09-25 21:49:37 -0600

    Third Commit

--- pages/views.py
+++ pages/views.py
@@ -16,3 +16,14 @@ class AboutPageView(TemplateView):
         context["contact_address"] = "123 Main Street"
         context["phone_number"] = "666-666-6666"
         return context
+    
+class ProductsPageView(TemplateView):
+    template_name = "products.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["product_1"] = "Thumbtacks"
+        context["product_2"] = "10 lb Wheel of Cheese"
+        context["product_3"] = "2004 Subaru Forester Engine Replacement"
+        context["product_4"] = "T-Shirt that says 'Howdy'"
+        return context
